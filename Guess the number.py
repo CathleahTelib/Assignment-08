43d42d2b361c5b89531421a6739753d03f2fbe9c
Cathleah Tabaquero <92615682+CathleahTelib@users.noreply.github.com>
2021-12-07 08:30:50 +0700

    Update Guess the number.py

--- Guess the number.py
+++ Guess the number.py
@@ -1,6 +1,7 @@
 import random
 
 inputNumber= random.randint(0,100)
+
 while True:
     print('Please guess the number between 0-100')
     guessNumber= int(input())
@@ -8,6 +9,6 @@ while True:
         print("You guessed it right!")
         print("The random number is",inputNumber)
     elif guessNumber > inputNumber:
-        print("Greater than")
+        print("The number you input is Greater than the random number")
     elif guessNumber < inputNumber:
-        print('Less than')
+        print('The number you input is Less than the random number')
